d68c50389e5af33d2cf7bc0f5ac6e7d936b4aac9
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-06-20 05:01:31 +0200

    register nbastatsapi in the project

--- APIs/settings.py
+++ APIs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nbastatsapi.apps.NbastatsapiConfig'
     'todoapi.apps.TodoapiConfig',
     'rest_framework',
     'django.contrib.admin',
