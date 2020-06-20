7f9392d1eeb9e05878f47415d507175d6d778d24
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-06-20 16:32:51 +0200

    importing jsonResponse

--- nbastatsapi/views.py
+++ nbastatsapi/views.py
@@ -1,23 +1,10 @@
-from django.http import HttpResponse as hp
+from django.http import JsonResponse as jr
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.exceptions import ParseError
-from rest_framework.parsers import FileUploadParser
-from rest_framework.views import APIView
-
-
-class MyUploadView(APIView):
-    def __init__(self):
-        self.parser_class = [FileUploadParser]
-
-    def put(self, req, format=None):
-        if 'file' not in req.FILES:
-            raise ParseError("Empty content")
-        print(req.FILES)
 
 
 @csrf_exempt
 def getAdvancedStats(req):
     if req.method == 'POST':
         print(req.FILES)
-        return hp('Successful')
-    return hp('No stats')
+        return jr('Successful')
+    return jr('No stats')
