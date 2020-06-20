16c5c96f0230f74d20ca18f699deb9759c4fdeec
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-06-20 16:31:14 +0200

    I don't know

--- APIs/urls.py
+++ APIs/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('nbastats/', include('nbastatsapi.urls')),
     path('', include('todoapi.urls'))
 ]
--- nbastatsapi/views.py
+++ nbastatsapi/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse as hp
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import FileUploadParser
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class MyUploadView(APIView):
+    def __init__(self):
+        self.parser_class = [FileUploadParser]
+
+    def put(self, req, format=None):
+        if 'file' not in req.FILES:
+            raise ParseError("Empty content")
+        print(req.FILES)
+
+
+@csrf_exempt
+def getAdvancedStats(req):
+    if req.method == 'POST':
+        print(req.FILES)
+        return hp('Successful')
+    return hp('No stats')
